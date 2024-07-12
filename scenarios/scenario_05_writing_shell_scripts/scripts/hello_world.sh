62378d8c7ab6894ea907bdf3a02b2600119ae51e
lalindrait <57055160+lalindrait@users.noreply.github.com>
2024-07-12 09:11:28 +0530

    add script

---
+++ scenarios/scenario_05_writing_shell_scripts/scripts/hello_world.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/bash
+
+# =================================================================
+# Script Name: hello_world.sh
+# Author: Lalindra
+# Email: user@example.com
+# Description: Prints hello world to the screen
+# Date: 2024-06-21
+# Version: 0.1 
+# =================================================================
+
+# Print Helll World
+echo "Hello World"
+
